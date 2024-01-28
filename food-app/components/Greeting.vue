d6c36a8a3bf996db8816ad71fe404bdd2b36e8a1
Indra <iwahyu@student.telkomuniversity.ac.id>
2024-01-28 08:50:57 +0700

    [Feat] Fixed Greeting Card background color

--- food-app/components/Greeting.vue
+++ food-app/components/Greeting.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="sticky top-0 flex items-center justify-start w-full bg-red-500 bg-dark-grey py-5">
+    <div class="sticky top-0 flex items-center justify-start w-full bg-dark-grey py-5">
         <p class="font-bold text-3xl">Hello, <span class="text-primary">Indra</span>!</p>
     </div>
 </template>
